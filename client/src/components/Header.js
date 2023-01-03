616d4042d54793690409f6b819edbf50aacea859
kundalili <105823291+kundalili@users.noreply.github.com>
2023-01-03 09:34:39 +0100

    app set up

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+<div>
+        <ul>
+            <li>Über mich</li>
+            <li>Referenzen</li>
+            <li>Kontakt</li>
+        </ul>
+    </div>
+  )
+}
